31d8d0918decd59899bcc385bf946abf5480d493
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-03-08 00:47:36 -0300

    add default codecompanion

--- after/plugin/codecompanion.lua
+++ after/plugin/codecompanion.lua
@@ -1,34 +1,50 @@
+local function preferred_model_picker(preferred)
+  return function(self)
+    if self == nil then
+      -- Debug:render() calls Adapter.schema.model.default() but doesn't pass
+      -- in `self`. Just return an empty string in this case.
+      return ''
+    end
+
+    local choices = self.schema.model.choices(self)
+
+    for _, best in ipairs(preferred) do
+      for _, choice in ipairs(choices) do
+        if choice:find(best, 1, true) then
+          return choice
+        end
+      end
+    end
+
+    return choices[1]
+  end
+end
+
+local ollama_models = {
+                'qwen2.5-coder:14b',
+                'qwen2.5-coder:7b',
+                'qwen2.5-coder:3b',
+                'qwen2.5-coder:1.5b',
+                'qwen2.5-coder:0.5b',
+              }
+
 require("codecompanion").setup({
 	adapters = {
-		-- ollama model config
-		qwen_7b = function()
-			return require("codecompanion.adapters").extend("ollama", {
-				name = "qwen_7b",
-				formatted_name = "Qwen2.5 7B",
-				env = {
-					url = "http://localhost:11434",
-				},
-				url = "${url}/api/chat",
-				schema = {
-					model = {
-						default = "qwen2.5-coder:7b"
-					},
-					num_ctx = {
-						default = 32768
-					},
-					num_predict = {
-						default = -1
-					}
-				}
-			})
-		end
-	},
+ollama = function()
+      return require("codecompanion.adapters").extend("ollama", {
+        schema = {
+          model = {
+            default = preferred_model_picker(ollama_models),
+          },
+        },
+      })
+    end,	},
 	strategies = {
 		chat = {
-			adapter = "qwen_7b",
+			adapter = "ollama",
 		},
 		inline = {
-			adapter = "qwen_7b",
+			adapter = "ollama",
 		},
 	},
 	display = {
@@ -46,7 +62,6 @@ require("codecompanion").setup({
 				position = "right",
 				width = .35,
 			},
-			start_in_insert_mode = true,
 		},
 		inline = {
 			layout = "horizontal"
@@ -59,5 +74,3 @@ vim.keymap.set({ "n", "v" }, "<leader>ct", "<cmd>CodeCompanionChat Toggle<cr>", 
 vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CodeCompanion<cr>", { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
--- Expand 'cc' into 'CodeCompanion' in the command line
-vim.cmd([[cab cc CodeCompanion]])
