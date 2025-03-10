56331d28ac4d1485c72db43a1c2bc6007e4fb43e
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-03-10 02:27:56 -0300

    add plugin to make telescope ui be able for all selections.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,16 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sf', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")});
-end)
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
 
+
+-- This is your opts table
+require("telescope").setup {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+      }
+    }
+  }
+}
+require("telescope").load_extension("ui-select")
