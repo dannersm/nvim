230e4d049c1c6fb80d579083df694b7e744119e0
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-03-16 03:33:01 -0300

    add indentation for lua and format packer.lua

--- lua/danner/packer.lua
+++ lua/danner/packer.lua
@@ -1,140 +1,142 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- fuzzy finder
-	use({
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.8',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	})
+    -- fuzzy finder
+    use({
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    })
 
-	use({ 'nvim-telescope/telescope-ui-select.nvim'})
+    use({ 'nvim-telescope/telescope-ui-select.nvim' })
 
-	-- colorscheme catppuccin ftw
-	use { "catppuccin/nvim", as = "catppuccin" }
+    -- colorscheme catppuccin ftw
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-	-- syntax highlighting
-	use("nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' })
+    -- syntax highlighting
+    use("nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' })
 
-	-- filetree
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons',
-		},
-	}
+    -- filetree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+        },
+    }
 
-	-- why not?
-	use("theprimeagen/harpoon")
+    -- why not?
+    use("theprimeagen/harpoon")
 
-	-- maximization
-	use {
-		'declancm/maximize.nvim',
-	}
-	-- status bar
-	use {
-		'nvim-lualine/lualine.nvim',
-	}
+    -- maximization
+    use {
+        'declancm/maximize.nvim',
+    }
+    -- status bar
+    use {
+        'nvim-lualine/lualine.nvim',
+    }
 
-	-- configure snippets
-	use {
-		"L3MON4D3/LuaSnip",
-		requires = { "rafamadriz/friendly-snippets" },
-	}
+    -- configure snippets
+    use {
+        "L3MON4D3/LuaSnip",
+    }
 
 
-	-- lsp management
-	use({
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	})
+    use { "rafamadriz/friendly-snippets" }
 
-	-- completion engine
-	use {
-		"hrsh7th/nvim-cmp",
-		requires = {
-			'neovim/nvim-lspconfig',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-cmdline',
-			'L3MON4D3/LuaSnip',
-			'saadparwaiz1/cmp_luasnip',
-		}
-	}
 
-	-- formatter
-	use {
-		"stevearc/conform.nvim"
-	}
+    -- lsp management
+    use({
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    })
 
-	-- autopairs
-	use {
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup()
-		end
-	}
+    -- completion engine
+    use {
+        "hrsh7th/nvim-cmp",
+        requires = {
+            'neovim/nvim-lspconfig',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        }
+    }
 
-	-- integration with tmux and better navigation
-	use {
-		"christoomey/vim-tmux-navigator",
-	}
+    -- formatter
+    use {
+        "stevearc/conform.nvim"
+    }
 
-	-- integration with tmux and better navigation
-	use {
-		"tpope/vim-obsession",
-	}
+    -- autopairs
+    use {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup()
+        end
+    }
 
-	-- molten
-	use({
-		"benlubas/molten-nvim",
-		tag = "v1.*",           -- equivalent to version "^1.0.0"
-		run = ":UpdateRemotePlugins", -- equivalent to build
-	})
+    -- integration with tmux and better navigation
+    use {
+        "christoomey/vim-tmux-navigator",
+    }
 
-	-- codecompanion for llm
-	use({
-		"olimorris/codecompanion.nvim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		}
-	})
+    -- integration with tmux and better navigation
+    use {
+        "tpope/vim-obsession",
+    }
 
-	-- project indexation with vectorcode
-	use {
-		"Davidyz/VectorCode",
-		requires = { "nvim-lua/plenary.nvim" }
-	}
+    -- molten
+    use({
+        "benlubas/molten-nvim",
+        tag = "v1.*",             -- equivalent to version "^1.0.0"
+        run = ":UpdateRemotePlugins", -- equivalent to build
+    })
 
-	-- markdown renderer
-	use({
-		'MeanderingProgrammer/render-markdown.nvim',
-		after = { 'nvim-treesitter' },
-		requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
-		config = function()
-		end,
-	})
+    -- codecompanion for llm
+    use({
+        "olimorris/codecompanion.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        }
+    })
 
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+    -- project indexation with vectorcode
+    use {
+        "Davidyz/VectorCode",
+        requires = { "nvim-lua/plenary.nvim" }
+    }
+
+    -- markdown renderer
+    use({
+        'MeanderingProgrammer/render-markdown.nvim',
+        after = { 'nvim-treesitter' },
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+        config = function()
+        end,
+    })
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
