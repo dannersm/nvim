52c703b2ffc38db36c1b9b10ad23a4b1b9017e41
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-03-16 01:44:01 -0300

    add different indentations on ftplugin

--- lua/danner/set.lua
+++ lua/danner/set.lua
@@ -1,11 +1,11 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.wrap = true
 vim.opt.smartcase = true
-vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.termguicolors = true
-vim.opt.backspace = "indent,eol,start"
 vim.opt.scrolloff = 10
