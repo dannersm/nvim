7c6826b68f7af5cf793c9d401dd2b2effa32268a
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-04-04 20:24:32 -0300

    change leader mapping to global in molten

--- after/plugin/molten.lua
+++ after/plugin/molten.lua
@@ -3,20 +3,20 @@ vim.g.molten_enter_output_behavior = "open_and_enter"
 vim.g.molten_virt_text_max_lines = 30
 vim.g.molten_wrap_output = true
 
-vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+vim.keymap.set("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
     { silent = true, desc = "evaluate visual selection" })
-vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>",
+vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>",
     { silent = true, desc = "evaluate line" })
-vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>",
+vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>",
     { silent = true, desc = "re-evaluate cell" })
-vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>",
+vim.keymap.set("n", "<leader>rd", ":MoltenDelete<CR>",
     { silent = true, desc = "molten delete cell" })
 
-vim.keymap.set("n", "<localleader>ho", ":MoltenHideOutput<CR>",
+vim.keymap.set("n", "<leader>ho", ":MoltenHideOutput<CR>",
     { silent = true, desc = "hide output" })
-vim.keymap.set("n", "<localleader>eo", ":noautocmd MoltenEnterOutput<CR>",
+vim.keymap.set("n", "<leader>eo", ":noautocmd MoltenEnterOutput<CR>",
     { silent = true, desc = "show/enter output" })
 
 
-vim.keymap.set("n", "<localleader>mr", ":MoltenRestart<CR>",
+vim.keymap.set("n", "<leader>mr", ":MoltenRestart<CR>",
     { silent = true, desc = "restart the kernel" })
