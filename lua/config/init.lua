d7c314c058038d7255fa0def84a27c4d85c74171
Danner Schlotterbeck <danner.schmen@gmail.com>
2025-11-06 22:38:30 -0300

    add molten support

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,6 +2,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- setup virtual env for molten
+vim.g.python3_host_prog=vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
+
 
 require("config.opts")
 require("config.lazy")
